c98b5c02ce7e5b2e0736ea5ff5f785fbce37b139
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-07-07 19:57:04 +0530

    Added routes for pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import {Switch, Route} from 'react-router-dom'
 
-import './App.css';
+import Home from './components/Home'
+import TopRated from './components/TopRated'
+import Upcoming from './components/Upcoming'
+import MovieDetails from './components/MovieDetails'
+import NotFound from './components/NotFound'
 
-const App = () => {}
+import './App.css'
 
-export default App;
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/top-rated" component={TopRated} />
+    <Route exact path="/upcoming" component={Upcoming} />
+    <Route exact path="/movie/:id" component={MovieDetails} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
