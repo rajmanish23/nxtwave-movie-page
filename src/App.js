bb9b39c3d969945ed7a8439dacc2f9584fa182b8
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-07-07 21:18:51 +0530

    Added a page for search results

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import TopRated from './components/TopRated'
 import Upcoming from './components/Upcoming'
 import MovieDetails from './components/MovieDetails'
 import NotFound from './components/NotFound'
+import SearchResults from './components/SearchResults'
 
 import './App.css'
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/search" component={SearchResults} />
     <Route exact path="/top-rated" component={TopRated} />
     <Route exact path="/upcoming" component={Upcoming} />
     <Route exact path="/movie/:id" component={MovieDetails} />
