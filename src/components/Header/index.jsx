3581d2e16159670fa3eceffc153317a554cd8f0e
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-07-07 21:40:57 +0530

    Started work on Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,30 @@
+import {useContext, useState} from 'react'
+import {Link, Redirect} from 'react-router-dom'
+
+import SearchContext from '../../context/SearchContext'
+
 import './index.css'
 
-const Header = () => <h1>Header component</h1>
+const Header = () => {
+  const {setSearchInputContext} = useContext(SearchContext)
+  const [userSearchInput, setUserSearchInput] = useState('')
+
+  const onChangeUpdateUserSearchInput = e => {
+    setUserSearchInput(e.target.value)
+  }
+
+  const onClickSearch = () => {
+    setSearchInputContext(userSearchInput)
+    return <Redirect to="/search" />
+  }
+
+  return (
+    <div className="header-bg-container">
+      <Link to="/" className="header-nav-link">
+        <h1 className="header-page-name">MovieDB</h1>
+      </Link>
+    </div>
+  )
+}
 
 export default Header
