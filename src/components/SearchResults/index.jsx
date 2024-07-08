16a9cb7ca2bf9bffd03fba99ce9a6800658385a4
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-07-08 07:08:27 +0530

    Added dummy JSX for Search Results route

---
+++ src/components/SearchResults/index.jsx
@@ -0,0 +1,14 @@
+import MovieList from '../MovieList'
+import Header from '../Header'
+
+import './index.css'
+
+const SearchResults = () => (
+  <>
+    <Header />
+    <h1>Search</h1>
+    <MovieList />
+  </>
+)
+
+export default SearchResults
