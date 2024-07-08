f8327d2e5c313e8807a1faad2739fe65bfb57fc8
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-07-08 09:23:03 +0530

    Added JSX for cast list sub component

--- src/components/MovieDetails/CastList/index.jsx
+++ src/components/MovieDetails/CastList/index.jsx
@@ -1,5 +1,26 @@
 import './index.css'
 
-const CastList = () => <h1>Cast List component</h1>
+const CastList = ({castData}) => (
+  <div className="cast-list-bg-container">
+    <h1 className="cast-list-heading">Cast</h1>
+    <ul className="cast-list-container">
+      {castData.map(eachCastObj => {
+        const {profileImage, name, id, character} = eachCastObj
+        const profileImageUrl = `https://image.tmdb.org/t/p/w500${profileImage}`
+        return (
+          <li className="cast-list-item-container" key={id}>
+            <img
+              className="cast-list-profile-image"
+              src={profileImageUrl}
+              alt={`Profile of ${name}`}
+            />
+            <p className="cast-list-detail-text">{name}</p>
+            <p className="cast-list-detail-text">{`Character: ${character}`}</p>
+          </li>
+        )
+      })}
+    </ul>
+  </div>
+)
 
 export default CastList
